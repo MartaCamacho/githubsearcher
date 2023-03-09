312a5ec30356f0870e830696bbca03420be39381
martacamacho <martacamachovargas@gmail.com>
2023-03-09 09:57:40 +0100

    added metatags

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,22 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Github Searcher</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          property="description"
+          name="description"
+          itemProp="description"
+          content="This is a github searcher to find the projects you want"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
