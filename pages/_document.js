8e3e963e6a8fba78a79a218985fc9e4d0b5c2ec2
martacamacho <martacamachovargas@gmail.com>
2023-03-09 20:34:07 +0100

    changed head to individual components

--- pages/_document.js
+++ pages/_document.js
@@ -3,16 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <title>Github Searcher</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta
-          property="description"
-          name="description"
-          itemProp="description"
-          content="This is a github searcher to find the projects you want"
-        />
-      </Head>
+      <Head/>
       <body>
         <Main />
         <NextScript />
